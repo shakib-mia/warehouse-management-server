d35eae4a1f7c135fd671a87b854633a8c1a5eafe
shakib-mia <smdshakibmia2001@gmail.com>
2022-04-30 10:01:23 +0600

    server side code made clean

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const express = require('express');
+const app = express();
 
 require('dotenv').config();
-const app = express();
 
 const port = process.env.PORT || 7000;
 
@@ -12,8 +13,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
       res.send("Hurray!!! server is running")
-})
-
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gv79d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
