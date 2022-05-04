cc223bb6bf5fc2c93e13a87e074667795c67bd5e
shakib-mia <smdshakibmia2001@gmail.com>
2022-05-04 08:40:13 +0600

    update method created

--- index.js
+++ index.js
@@ -37,6 +37,12 @@ app.get('/cars/:_id', async (req, res) => {
       res.send(specialCar)
 })
 
+app.put("/cars/:_id", (req, res) => {
+      const newQuantity = req.body;
+      const result = collection.updateOne(newQuantity);
+      res.send(result)
+})
+
 app.get("/comments", async (req, res) => {
       const query = {};
       const cursor = allComments.find(query);
