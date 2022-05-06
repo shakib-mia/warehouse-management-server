f0469bfb35d871cc2cf1e6a6b8ea30da3f50d984
shakib-mia <smdshakibmia2001@gmail.com>
2022-05-06 17:03:21 +0600

    quantity can be increased from front end

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const express = require('express');
 const app = express();
@@ -18,52 +18,67 @@ app.get('/', (req, res) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gv79d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect();
-const collection = client.db('luxuriousCar').collection('allCars');
-const allComments = client.db('luxuriousCar').collection('comments');
+async function run() {
+      try {
+            const collection = client.db('luxuriousCar').collection('allCars');
+            const allComments = client.db('luxuriousCar').collection('comments');
 
-app.get('/allCars', async (req, res) => {
-      const query = {};
-      const cursor = collection.find(query);
-      const cars = await cursor.toArray();
-      res.send(cars)
-})
+            client.connect();
+            app.get('/allCars', async (req, res) => {
+                  const query = {};
+                  const cursor = collection.find(query);
+                  const cars = await cursor.toArray();
+                  res.send(cars)
+            })
 
-// app.post('/allCars', async (req, res) => {
-//       const newCar = req.body;
-//       const result = await collection.insertOne(newCar);
-//       res.send(result)
-// })
-app.put('/allCars', async (req, res) => {
-      const newCar = req.body;
-      const result = await collection.insertOne(newCar);
-      res.send(result)
-})
+            app.put('/allCars', async (req, res) => {
+                  const newCar = req.body;
+                  const result = await collection.insertOne(newCar);
+                  res.send(result)
+            })
 
-app.get("/comments", async (req, res) => {
-      const query = {};
-      const cursor = allComments.find(query);
-      const comments = await cursor.toArray();
-      res.send(comments)
-})
+            app.get("/comments", async (req, res) => {
+                  const query = {};
+                  const cursor = allComments.find(query);
+                  const comments = await cursor.toArray();
+                  res.send(comments)
+            })
 
-app.post("/comments", async (req, res) => {
-      const newComment = req.body;
-      const result = await allComments.insertOne(newComment)
-      res.send(result)
-})
+            app.post("/comments", async (req, res) => {
+                  const newComment = req.body;
+                  const result = await allComments.insertOne(newComment)
+                  res.send(result)
+            })
 
-app.get('/allCars/:_id', async (req, res) => {
-      const query = {};
-      const cursor = collection.find(query);
-      const cars = await cursor.toArray();
-      const specialCar = cars.find(car => car._id == req.params._id);
-      res.send(specialCar)
-})
+            app.get('/allCars/:_id', async (req, res) => {
+                  const query = {};
+                  const cursor = collection.find(query);
+                  const cars = await cursor.toArray();
+                  const specialCar = cars.find(car => car._id == req.params._id);
+                  res.send(specialCar)
+            })
 
-app.patch("/allCars/:_id", async (req, res) => {
-      const updatedItem = req.body;
-      console.log(updatedItem.quantity);
-})
+            app.put("/allCars/:_id", async (req, res) => {
+                  console.log(req)
+
+                  const id = req.params._id;
+                  const filter = { _id: ObjectId(id) };
+                  const update = {
+                        $set: {
+                              quantity: req.body.quantity
+                        }
+                  }
+
+
+
+                  const result = await collection.updateOne(filter, update)
+                  console.log("message for updating", result)
+                  res.send(result)
+            })
+      }
+      finally { }
+}
+
+run().catch(console.dir)
 
 app.listen(port, () => console.log("listening to port", port))
